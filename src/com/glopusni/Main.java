15fffb578a99176b458b7d80cb730346a21818c5
Gabor Lopusni <g.lopusni@t-systems.com>
2022-12-01 15:11:32 +0100

    add check

--- src/com/glopusni/Main.java
+++ src/com/glopusni/Main.java
@@ -1,6 +1,7 @@
 package com.glopusni;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,10 +11,11 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         List<Integer> calories = new ArrayList<>();
         int max = Integer.MIN_VALUE;
-        if(args.length == 0) {
+
+        if (args.length == 0 || !(new File(args[0]).isFile())) {
             System.out.println("No parameters provided.");
             return;
         }
@@ -23,24 +25,24 @@ public class Main {
             int tempMax = 0;
 
             while ((value = reader.readLine()) != null) {
-                try{
+                try {
                     tempMax += Integer.parseInt(value);
                 } catch (NumberFormatException e) {
                     max = Math.max(tempMax, max);
                     calories.add(tempMax);
                     tempMax = 0;
-                    System.out.println("Empty line found.");
                 }
 
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         System.out.println("Max calories among the elves: " + max);
         System.out.println("Calories sum for the first three elves:" + calculateTheGreatestThreeSum(calories));
     }
 
-    private static int calculateTheGreatestThreeSum(List<Integer> calories){
+    private static int calculateTheGreatestThreeSum(List<Integer> calories) {
         calories = calories.stream().sorted().collect(Collectors.toList());
         return calories.get(calories.size() - 1) + calories.get(calories.size() - 2) + calories.get(calories.size() - 3);
     }
