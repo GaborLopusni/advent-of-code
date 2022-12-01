340df0c654ea2646986433f62b296b2fa9df004b
Gabor Lopusni <g.lopusni@t-systems.com>
2022-12-01 14:56:56 +0100

    working version

---
+++ src/com/glopusni/Main.java
@@ -0,0 +1,47 @@
+package com.glopusni;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Integer> calories = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        if(args.length == 0) {
+            System.out.println("No parameters provided.");
+            return;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            String value;
+            int tempMax = 0;
+
+            while ((value = reader.readLine()) != null) {
+                try{
+                    tempMax += Integer.parseInt(value);
+                } catch (NumberFormatException e) {
+                    max = Math.max(tempMax, max);
+                    calories.add(tempMax);
+                    tempMax = 0;
+                    System.out.println("Empty line found.");
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Max calories among the elves: " + max);
+        System.out.println("Calories sum for the first three elves:" + calculateTheGreatestThreeSum(calories));
+    }
+
+    private static int calculateTheGreatestThreeSum(List<Integer> calories){
+        calories = calories.stream().sorted().collect(Collectors.toList());
+        return calories.get(calories.size() - 1) + calories.get(calories.size() - 2) + calories.get(calories.size() - 3);
+    }
+}
